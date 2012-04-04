e1ebf4970d661b6c6aa4d82b747db99209a10141
Tomas McCandless <tomas@cs.utexas.edu>
2012-04-03 22:07:06 -0500

    satisfactorily completed the unigram model

--- pa2-autocorrect-v1/python/UniformLanguageModel.py
+++ pa2-autocorrect-v1/python/UniformLanguageModel.py
@@ -5,6 +5,10 @@ class UniformLanguageModel:
   def __init__(self, corpus):
     """Initialize your data structures in the constructor."""
     self.words = set([])
+    # map words to their counts
+    self.counts = {}
+    # map words to their unigram probabilities
+    self.probs = {}
     self.train(corpus)
 
   def train(self, corpus):
@@ -15,15 +19,33 @@ class UniformLanguageModel:
       for datum in sentence.data: # iterate over datums in the sentence
         word = datum.word # get the word
         self.words.add(word)
+        self.update_count(word)
+    
+    self.smooth()
+
+  # track the raw counts of unigrams
+  def update_count(self, word):
+    if word in self.counts:
+      self.counts[word] += 1
+    else:
+      self.counts[word] = 1
+
+  # use the raw counts to compute add-1 smoothed unigram probabilities
+  def smooth(self):
+    for word in self.counts:
+      self.probs[word] = (self.counts[word] + 1.0) / len(self.words)
+
+  # should treat a word not in the corpus as if it was seen once
+  def get_prob(self, word):
+    return self.probs[word] if word in self.probs else 1.0 / len(self.words)
 
   def score(self, sentence):
     """ Takes a list of strings as argument and returns the log-probability of the 
         sentence using your language model. Use whatever data you computed in train() here.
     """
     score = 0.0
-    probability = math.log(1.0/len(self.words))
     for token in sentence: # iterate over words in the sentence
-      score += probability
+      score += math.log(self.get_prob(token))
     # NOTE: a simpler method would be just score = sentence.size() * - Math.log(words.size()).
     # we show the 'for' loop for insructive purposes.
     return score
