17c6842a66307c14df1e27b59366cad4b1ce7779
Tomas McCandless <tomas@cs.utexas.edu>
2012-04-09 20:56:05 -0500

    nearly completed with pa2, some decent work on sentiment

--- pa2-autocorrect-v1/python/CustomLanguageModel.py
+++ pa2-autocorrect-v1/python/CustomLanguageModel.py
@@ -10,7 +10,10 @@ class CustomLanguageModel:
     self.metacounts = {}
     # map unigrams to their Good-Turing probabilities
     self.probs = {}
+    self.avg_count_ratio = 0.0
+    self.debug = True
     self.train(corpus)
+    
   
   # track the raw counts of unigrams
   def update_count(self, word):
@@ -34,6 +37,11 @@ class CustomLanguageModel:
   def get_metacount(self, count):
     return self.metacounts[count] if count in self.metacounts else 0
 
+  def get_approx_metacount(self, count):
+    if (self.debug):
+      print "approx_metacount for %d: %d" % (count, self.get_metacount(1) * 1.0 / self.avg_count_ratio**(count -1))
+    return self.get_metacount(1) * 1.0 / self.avg_count_ratio**(count -1)
+
   # compute the count counts
   def compute_metacounts(self):
     for word in self.counts:
@@ -47,13 +55,19 @@ class CustomLanguageModel:
   # c* = (c + 1) * N_{c+1} / N_{c}
   def normalize(self, word):
     wcount = self.get_count(word)
-    cstar = (wcount + 1) * self.get_metacount(wcount + 1) * 1.0 / self.get_metacount(wcount)
+    cstar = (wcount + 1) * self.get_approx_metacount(wcount + 1) * 1.0 / self.get_approx_metacount(wcount)
     assert cstar != 0.0, "cstar is 0. word: %s word_count: %d N_{c+1}: %d" % (word, wcount, self.get_metacount(wcount + 1))
     return cstar
 
   def get_prob(self, word):
     return self.probs[word] if word in self.probs else self.normalize(word)
 
+  def find_min_zero(self):
+    i = 1
+    while self.get_metacount(i) > 0:
+      i = i + 1
+    return i
+  
   def train(self, corpus):
     """ Takes a corpus and trains your language model. 
         Compute any counts or other corpus statistics in this function.
@@ -64,9 +78,28 @@ class CustomLanguageModel:
         self.update_count(word)
 
     self.compute_metacounts()
+
+    print self.find_min_zero()
+
+    i1 = self.get_metacount(1)
+    ratios = []
+    for k in sorted(self.metacounts.keys())[1:10]:
+      #print k
+      i2 = self.get_metacount(k)
+      r = 1.0 * i1 / i2
+      ratios.append(r)
+      i1 = i2
+    print ratios
+    self.avg_count_ratio = sum(ratios) / len(ratios)
+    
     for key in sorted(self.metacounts.keys()):
-        print "f(%d) = %d" % (key, self.metacounts[key])
+        print "f(%d) = %d    g(%d) = %d" % (key, self.get_metacount(key), key, self.get_approx_metacount(key))
+    
+    print self.avg_count_ratio
+    
+    print self.avg_count_ratio
     self.compute_probs()
+    self.debug = False
 
   def score(self, sentence):
     """ Takes a list of strings as argument and returns the log-probability of the 
